9053607ff1c100d09bd79f837c9bda3a5f9d4ec3
FangjieHung <j87345@gmail.com>
2026-09-26 07:44:41 +0800

    test: TEMP break one assertion to prove CI e2e goes red (do not merge)

--- apps/admin-e2e/src/e2e/app.cy.ts
+++ apps/admin-e2e/src/e2e/app.cy.ts
@@ -13,6 +13,6 @@ describe('admin-e2e', () => {
 
     // 產品名稱留在工作台外框，每個頁面只有一個屬於自己內容的 h1。
     cy.get('.app-sidenav').should('be.visible').and('contain.text', 'AI 助理工作台');
-    cy.get('h1').should('have.length', 1).and('be.visible').and('contain.text', '首頁');
+    cy.get('h1').should('have.length', 1).and('be.visible').and('contain.text', 'THIS-TEXT-DOES-NOT-EXIST');
   });
 });
